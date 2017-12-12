1a5903cf3cc50eda5bad982f07bcdddc6f9d5653
Esdras Henrique <esdrasha1@gmail.com>
2017-12-12 14:35:41 -0300

    Update do projeto

---
+++ public_html/js.js
@@ -0,0 +1,44 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function jogo() {
+
+//pega o id e muda para a imagem na parte de cima segundo o code table
+document.getElementById("p11").innerHTML = "&#9820";
+document.getElementById("p12").innerHTML = "&#9822;";
+document.getElementById("p13").innerHTML = "&#9821;";
+document.getElementById("p14").innerHTML = "&#9819;";
+document.getElementById("p15").innerHTML = "&#9818;";
+document.getElementById("p16").innerHTML = "&#9821;";
+document.getElementById("p17").innerHTML = "&#9822;";
+document.getElementById("p18").innerHTML = "&#9820;";
+document.getElementById("p21").innerHTML = "&#9823;";
+document.getElementById("p22").innerHTML = "&#9823;";
+document.getElementById("p23").innerHTML = "&#9823;";
+document.getElementById("p24").innerHTML = "&#9823;";
+document.getElementById("p25").innerHTML = "&#9823;";
+document.getElementById("p26").innerHTML = "&#9823;";
+document.getElementById("p27").innerHTML = "&#9823;";
+document.getElementById("p28").innerHTML = "&#9823;";
+
+//pega o id e muda para a imagem na parte de baixo segundo o code table
+document.getElementById("p81").innerHTML = "&#9814;";
+document.getElementById("p82").innerHTML = "&#9816;";
+document.getElementById("p83").innerHTML = "&#9815;";
+document.getElementById("p84").innerHTML = "&#9813;";
+document.getElementById("p85").innerHTML = "&#9812;";
+document.getElementById("p86").innerHTML = "&#9815;";
+document.getElementById("p87").innerHTML = "&#9816;";
+document.getElementById("p88").innerHTML = "&#9814;";
+document.getElementById("p71").innerHTML = "&#9817;";
+document.getElementById("p72").innerHTML = "&#9817;";
+document.getElementById("p73").innerHTML = "&#9817;";
+document.getElementById("p74").innerHTML = "&#9817;";
+document.getElementById("p75").innerHTML = "&#9817;";
+document.getElementById("p76").innerHTML = "&#9817;";
+document.getElementById("p77").innerHTML = "&#9817;";
+document.getElementById("p78").innerHTML = "&#9817;";
+
+}
